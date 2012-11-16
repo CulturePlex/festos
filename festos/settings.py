30b2e0cb9752ed47588e911f1818ddfda9113b70
Roberto Ulloa <roberto.ur@gmail.com>
2012-11-16 11:37:14 -0500

    missing some INSTALLED_APPLICATION in settings and the configuration of media and static for the server

--- festos/settings.py
+++ festos/settings.py
@@ -49,22 +49,31 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = join(PROJECT_ROOT,'../media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static/'
+STATIC_ROOT = join(PROJECT_ROOT,'../static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    join(PROJECT_ROOT, 'static'),
+)
+
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -124,10 +133,13 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'pipeline',
-    'djcelery',
+    'pipeline',        # necessary for compression and docviewer templates
+    'djcelery',        # necessary for python manage.py celery worker
+    'celery_haystack', # necessary for automatic rebuild_index
+    'haystack',        # necessary for manual rebuild_index
     'docviewer',
     'books',
+
 )
 
 # A sample logging configuration. The only tangible logging
