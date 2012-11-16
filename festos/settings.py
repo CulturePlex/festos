7b07b7244ee1d41c8918b827a58b7ce893090e10
Roberto Ulloa <roberto@laboratorio-desktop.(none)>
2012-11-16 15:44:44 -0500

    defining the parent directory as the project dir

--- festos/settings.py
+++ festos/settings.py
@@ -1,7 +1,11 @@
 # Django settings for demoproject project.
-from os.path import abspath, dirname, join
-PROJECT_ROOT = dirname(abspath(__file__))
+from os.path import abspath, dirname, join, pardir
 
+# the root project should be the folder before so there
+# can be shared statics living in festos site folder
+# there are two pardir because the first remove the settings.py
+# and the second one got to the actual parent
+PROJECT_ROOT = abspath(join(abspath(__file__), pardir, pardir))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -49,7 +53,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = join(PROJECT_ROOT,'../media')
+MEDIA_ROOT = join(PROJECT_ROOT,'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,27 +64,19 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = join(PROJECT_ROOT,'../static')
+STATIC_ROOT = join(PROJECT_ROOT,'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    join(PROJECT_ROOT, 'static'),
-)
-
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    join(PROJECT_ROOT, 'static'),
+    join(dirname(abspath(__file__)), 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -120,7 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    join(PROJECT_ROOT, 'templates'),
+    join(dirname(abspath(__file__)), 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -192,7 +188,7 @@ HAYSTACK_CONNECTIONS = {
 
 # Docviewer Configuration
 from docviewer.pipeline import *
-DOCVIEWER_DOCUMENT_ROOT = join(MEDIA_ROOT,'../docs/')
+DOCVIEWER_DOCUMENT_ROOT = join(MEDIA_ROOT,'docs/')
 DOCVIEWER_DOCUMENT_URL = '/docs/'
 DOCVIEWER_IMAGE_FORMAT =  'png'
 
