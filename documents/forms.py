e6690261ee3bc46814941d4cb8b6c2cf3c435c67
Roberto Ulloa <roberto.ur@gmail.com>
2013-04-11 16:11:24 -0400

    putting widgets in widgets.py, language widget readdy

--- documents/forms.py
+++ documents/forms.py
@@ -1,13 +1,11 @@
-import os
 from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 from django.forms.util import flatatt
 from django.utils.safestring import mark_safe
 from docviewer.forms import DocumentForm as Docviewer_DocumentForm
 from models import Document
-
+from widgets import AnchorField
 
 class DocumentAdminForm(Docviewer_DocumentForm):
     class Meta:
@@ -48,40 +46,14 @@ class DocumentForm(Docviewer_DocumentForm):
         return inst
 
 
-
-class AnchorOnlyWidget(forms.Widget):
-    def render(self, name, value, attrs):
-        final_attrs = self.build_attrs(attrs, name=name)
-        if hasattr(self, 'initial'):
-            value = self.initial
-        return mark_safe(
-            "<p style='padding-top:4px'><a href='%s' %s>%s</a></p>" %
-                (os.path.join(settings.MEDIA_ROOT,value.name),
-                flatatt(final_attrs),
-                value.name.split('/')[-1]))
-
-    def _has_changed(self, initial, data):
-        return False
-
-class AnchorOnlyField(forms.FileField):
-    widget = AnchorOnlyWidget
-    def __init__(self, widget=None, label=None, initial=None, help_text=None):
-        forms.Field.__init__(self, label=label, initial=initial,
-            help_text=help_text, widget=widget)
-
-    def clean(self, value, initial):
-        self.widget.initial = initial
-        return initial
-
-
-
 class EditDocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ('docfile','language', 'source', 'public', 'notes', )
 
-    docfile = AnchorOnlyField()
-    language = forms.CharField(required=False, help_text=None)
+    docfile = AnchorField()
+    language = forms.ChoiceField(
+          choices=Document.LANGUAGES, required=False, help_text=None)
     source = forms.CharField(required=False, help_text=None)
     public = forms.BooleanField(
         label=_('Publicly available'), required=False, help_text=None)
@@ -95,8 +67,7 @@ class EditDocumentForm(forms.ModelForm):
         super(EditDocumentForm, self).__init__(*args, **kwargs)
         instance = getattr(self, 'instance', None)
         if instance and instance.pk:
-            self.fields['language'].widget.attrs['readonly'] = True
-#            self.fields['docfile'].widget.attrs['readonly'] = True
+            self.fields['language'].widget.attrs['disabled'] = True
 
 
     def clean_language(self):
@@ -105,16 +76,6 @@ class EditDocumentForm(forms.ModelForm):
             return instance.language
         else:
             return self.cleaned_data['language']
-
-
-#    def clean_docfile(self):
-#        instance = getattr(self, 'instance', None)
-#        if instance and instance.pk:
-#            return instance.docfile
-#        else:
-#            return self.cleaned_data['docfile']
-
-
 
 
 
---
+++ documents/widgets.py
@@ -0,0 +1,30 @@
+import os
+from django import forms
+from django.conf import settings
+from django.forms.util import flatatt
+from django.utils.safestring import mark_safe
+
+class AnchorWidget(forms.Widget):
+    def render(self, name, value, attrs):
+        final_attrs = self.build_attrs(attrs, name=name)
+        if hasattr(self, 'initial'):
+            value = self.initial
+        return mark_safe(
+            "<p style='padding-top:4px'><a href='%s' %s>%s</a></p>" %
+                (os.path.join(settings.MEDIA_ROOT,value.name),
+                flatatt(final_attrs),
+                value.name.split('/')[-1]))
+
+    def _has_changed(self, initial, data):
+        return False
+
+
+class AnchorField(forms.FileField):
+    widget = AnchorWidget
+    def __init__(self, widget=None, label=None, initial=None, help_text=None):
+        forms.Field.__init__(self, label=label, initial=initial,
+            help_text=help_text, widget=widget)
+
+    def clean(self, value, initial):
+        self.widget.initial = initial
+        return initial
