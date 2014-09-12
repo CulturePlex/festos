12dec49c26e4062f0d9c0fa89f8e5861931a9791
mavillard <ajmavillard@gmail.com>
2014-09-12 15:32:49 -0400

    calling 'progress/' instead of 'progress'

--- documents/static/js/document_list.js
+++ documents/static/js/document_list.js
@@ -196,7 +196,7 @@ $(document).ready(function(){
     function progress(doc_ids) {
       $.ajax({
         type: "GET",
-        url: "progress",
+        url: "progress/",
         data: {'ids': doc_ids},
         dataType: 'json',
         success: function (payload) {
