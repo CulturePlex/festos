1eb13fb67cc72b19a7de6160ab69042169f4bd9d
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2014-10-28 16:27:49 -0400

    Test with User and Profile creation

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User
 from django.test import TestCase
 
+from accounts.models import Profile
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+#class SimpleTest(TestCase):
+#    def test_basic_addition(self):
+#        """
+#        Tests that 1 + 1 always equals 2.
+#        """
+#        self.assertEqual(1 + 1, 2)
+
+class UserProfileTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username='antonio',
+            password='antonio_password',
+            email='antonio@email.com',
+        )
+        self.profile = Profile.objects.create(user=self.user)
+    
+    def test_user_creation(self):
+        self.assertIsNotNone(self.user)
+        self.assertIsNotNone(self.user.username)
+        self.assertIsNotNone(self.user.password)
+        self.assertIsNotNone(self.user.email)
+    
+    def test_profile_creation(self):
+        self.assertIsNotNone(self.profile)
+        self.assertEqual(self.profile.user, self.user)
