e83a5e60777d16f6feec20ee35860b478e40626c
Antonio Jimenez-Mavillard <ajimene6@uwo.ca>
2014-11-17 15:30:59 -0500

    Some document tests

--- documents/tests.py
+++ documents/tests.py
@@ -1,6 +1,55 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+#from django.test import TestCase
+#from django.contrib.auth.models import User
 
-Replace this with more appropriate tests for your application.
-"""
+#from accounts.tests import create_user, exists_user, get_user
+#from documents.models import Document
+
+
+#def create_document(title, username):
+#    if exists_user(username):
+#        user = get_user(username)
+#    else:
+#        user = create_user(username)
+#    return Document.objects.create(
+#        title=title,
+#        owner=user,
+#    )
+
+#def get_document(title):
+#    return Document.objects.get(title=title)
+
+#def exists_document(title):
+#    return Document.objects.filter(title=title).exists()
+
+
+#class DocumentTest(TestCase):
+#    def setUp(self):
+#        doc = create_document('doc', 'antonio')
+#    
+#    def test_document_creation(self):
+#        user = get_user('antonio')
+#        doc = get_document('doc')
+#        
+#        self.assertIsNotNone(doc)
+#        self.assertIsNotNone(doc.id)
+#        self.assertIsNotNone(doc.title)
+#        self.assertIsNotNone(doc.owner)
+#        self.assertEqual(doc.user, user)
+#    
+#    def test_document_edition(self):
+#        doc = get_document('doc')
+#        new_title = 'story'
+#        doc.title = new_title
+#        doc.save()
+#        
+#        self.assertEqual(doc.title, new_title)
+#    
+#    def test_document_deletion(self):
+#        doc = get_document('doc')
+#        doc.delete()
+#        
+#        self.assertIsNone(user.id)
+#        
+#        doc_exists = exists_document('doc')
+#        
+#        self.assertFalse(doc_exists)
