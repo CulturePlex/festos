40be6b0f0f0830f7f5dfcf201f9808ce6069eb65
mavillard <ajimene6@uwo.ca>
2015-01-25 21:47:54 -0500

    Testing generate_document as a function but not as a task

--- festos/test_settings.py
+++ festos/test_settings.py
@@ -3,8 +3,12 @@ from settings import *
 MEDIA_ROOT = '/tmp/media'
 DOCVIEWER_DOCUMENT_ROOT = join(MEDIA_ROOT, 'docs/')
 
-CELERY_EAGER_PROPAGATES_EXCEPTIONS=True
-CELERY_ALWAYS_EAGER=True
-BROKER_BACKEND='memory'
+#STATICFILES_STORAGE = 'django_pipeline_forgiving.storages.PipelineForgivingStorage'
+STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineStorage'
+PIPELINE_ENABLED = False
+
+#CELERY_EAGER_PROPAGATES_EXCEPTIONS=True
+#CELERY_ALWAYS_EAGER=True
+#BROKER_BACKEND='default'
 
 #TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
