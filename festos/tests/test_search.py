5ee04309a996bf3d9072e2480c5f7c1ddfbd9b1f
Antonio Jimenez-Mavillard <ajimene6@uwo.ca>
2015-02-14 13:59:13 -0500

    Testing tags and search

---
+++ festos/tests/test_search.py
@@ -0,0 +1,177 @@
+#import uuid
+
+#from django.conf import settings
+#from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+#from django.core.urlresolvers import reverse
+#from django.db.models.signals import post_delete, post_save
+#from selenium.webdriver import ActionChains
+#from splinter import Browser
+#from userena.managers import UserenaManager
+
+#from documents import fss_utils as fss
+#from documents.models import Document
+#from documents.tests import disconnect
+#from docviewer.models import document_delete, document_save
+#from utils import create_user, exists_tag, get_document, get_tag
+#from test_user import check_permissions, login
+#from test_document import upload, set_site, process_document, get_abs_path
+
+
+#class SearchTest(StaticLiveServerTestCase):
+#    def setUp(self):
+#        fss.remove_tree(settings.MEDIA_ROOT)
+#        disconnect(post_save, document_save)
+#        check_permissions()
+#        set_site(self.live_server_url)
+#        
+#        self.browser = Browser()
+#        self.browser.visit(self.live_server_url)
+#        
+#        login_url = settings.LOGIN_URL
+#        self.browser.click_link_by_partial_href(login_url)
+#        
+#        username = 'antonio'
+#        password = 'antonio'
+#        user = create_user(username)
+#        user.staff = True
+#        user.superuser = True
+#        login(
+#            self.browser,
+#            username,
+#            password,
+#        )
+#        
+#        upload_url = reverse('documents.views.add_document')
+#        self.browser.click_link_by_partial_href(upload_url)
+#        
+#        source = 'local'
+#        docfile = get_abs_path('doctest.pdf')
+#        language = 'eng'
+#        public = True
+#        title = 'test'
+#        notes = 'test notes'
+#        upload(
+#            self.browser,
+#            source,
+#            docfile,
+#            language,
+#            public,
+#            title,
+#            notes,
+#        )
+#        
+#        document = get_document(title)
+#        process_document(document.id)
+#        self.browser.is_element_not_present_by_value('ready', 10)
+#        
+#        self.title = title
+#        import time; time.sleep(1)
+#    
+#    def test_search_title(self):
+#        self.browser.visit(self.live_server_url)
+#        
+#        title = 'test'
+#        
+#        driver = self.browser.driver
+#        actions = ActionChains(driver)
+#        searchbar_xpath = '//*[@id="search"]/div/div/div[2]'
+#        searchbar_div = driver.find_element_by_xpath(searchbar_xpath)
+#        actions.move_to_element(searchbar_div)
+#        actions.click()
+#        actions.perform()
+#        
+#        menu_title_xpath = '/html/body/ul/li[4]/a'
+#        menu_title = self.browser.find_by_xpath(menu_title_xpath)
+#        menu_title.click()
+#        input_title_xpath = \
+#            '//*[@id="search"]/div/div/div[2]/div[2]/div[2]/input'
+#        input_title = self.browser.find_by_xpath(input_title_xpath)
+#        input_title.type(title + '\r')
+#        
+#        search_list_url = \
+#            self.live_server_url + '/?title=' + title + '&'
+#        self.assertEquals(self.browser.url, search_list_url)
+#        
+#        summary_xpath = '/html/body/div/div[2]/p/small'
+#        summary = self.browser.find_by_xpath(summary_xpath)
+#        self.assertEquals(summary.value, '1 documents found')
+#        
+#        document_img_xpath = '/html/body/div/div[2]/ul/li/a/img'
+#        document_img = self.browser.find_by_xpath(document_img_xpath).click()
+#        viewer_title_xpath = (
+#            '//*[@id="documentviewer-container"]'
+#            '/div/div[1]/div[1]/div[1]/div[2]/h4/a'
+#        )
+#        viewer_title = self.browser.find_by_xpath(viewer_title_xpath)
+#        self.assertEquals(viewer_title.value, self.title)
+#        
+##        import time; time.sleep(3)
+#        self.browser.quit()
+#    
+#    def test_search_text(self):
+#        self.browser.visit(self.live_server_url)
+#        
+#        text = 'download'
+#        
+#        driver = self.browser.driver
+#        actions = ActionChains(driver)
+#        searchbar_xpath = '//*[@id="search"]/div/div/div[2]'
+#        searchbar_div = driver.find_element_by_xpath(searchbar_xpath)
+#        actions.move_to_element(searchbar_div)
+#        actions.click()
+#        actions.perform()
+#        
+#        menu_text_xpath = '/html/body/ul/li[3]/a'
+#        menu_text = self.browser.find_by_xpath(menu_text_xpath)
+#        menu_text.click()
+#        input_text_xpath = \
+#            '//*[@id="search"]/div/div/div[2]/div[2]/div[2]/input'
+#        input_text = self.browser.find_by_xpath(input_text_xpath)
+#        input_text.type(text + '\r')
+#        
+#        search_list_url = \
+#            self.live_server_url + '/?q=' + text + '&'
+#        self.assertEquals(self.browser.url, search_list_url)
+#        
+#        summary_xpath = '/html/body/div/div[2]/p/small'
+#        summary = self.browser.find_by_xpath(summary_xpath)
+#        self.assertEquals(summary.value, '1 documents found')
+#        
+#        page_xpath = '/html/body/div/div[2]/ul/li[1]/div[2]/div/div[2]/a/div'
+#        page_div = self.browser.find_by_xpath(page_xpath)
+#        self.assertIn(text, page_div.value)
+#        
+#        document_img_xpath = '/html/body/div/div[2]/ul/li/a/img'
+#        document_img = self.browser.find_by_xpath(document_img_xpath).click()
+#        viewer_title_xpath = (
+#            '//*[@id="documentviewer-container"]'
+#            '/div/div[1]/div[1]/div[1]/div[2]/h4/a'
+#        )
+#        viewer_title = self.browser.find_by_xpath(viewer_title_xpath)
+#        self.assertEquals(viewer_title.value, self.title)
+#        
+##        import time; time.sleep(3)
+#        self.browser.quit()
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
+#        
--- festos/tests/test_tag.py
+++ festos/tests/test_tag.py
@@ -1,113 +1,144 @@
-#import uuid
+import uuid
 
-#from django.conf import settings
-#from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-#from django.core.urlresolvers import reverse
-#from django.db.models.signals import post_delete, post_save
-#from splinter import Browser
-#from userena.managers import UserenaManager
+from django.conf import settings
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core.urlresolvers import reverse
+from django.db.models.signals import post_delete, post_save
+from selenium.webdriver import ActionChains
+from splinter import Browser
+from userena.managers import UserenaManager
 
-#from documents import fss_utils as fss
-#from documents.models import Document
-#from documents.tests import disconnect
-#from docviewer.models import document_delete, document_save
-#from utils import create_user, create_profile, exists_tag
-#from test_signin_up import check_permissions, login
-#from test_upload import upload, set_url_site, process_document
-
-
-#class EditTest(StaticLiveServerTestCase):
-#    def setUp(self):
-#        fss.remove_tree(settings.MEDIA_ROOT)
-#        disconnect(post_save, document_save)
-##        disconnect(post_delete, document_delete)
-#        check_permissions()
-#        set_url_site('localhost:8081')
-#        
-#        username = 'antonio'
-#        password = 'antonio'
-#        
-#        user = create_user(username)
-#        profile = create_profile(password)
-#        
-#        self.browser = Browser()
-#        self.browser.visit(self.live_server_url)
-#        login(
-#            self.browser,
-#            settings.LOGIN_URL,
-#            username,
-#            password,
-#        )
-#        
-#        source = 'local'
-#        docfile = '/home/antonio/Downloads/995.pdf'
-#        language = 'eng'
-#        public = False
-#        title = 'test'
-#        notes = 'test notes'
-#        
-#        upload(
-#            self.browser,
-#            reverse('documents.views.add_document'),
-#            source,
-#            docfile,
-#            language,
-#            public,
-#            title,
-#            notes,
-#        )
-#        
-#        document_xpath = '/html/body/div/div[2]/table/tbody/tr[1]'
-#        document_tr = self.browser.find_by_xpath(document_xpath)
-#        document_id = document_tr['data-id']
-#        process_document(document_id)
-#        
-#        self.browser.is_element_present_by_value('ready', 10)
-#    
-#    def test_add_tag(self):
-#        self.browser.visit(self.live_server_url)
-#        
-#        tag = 'tag'
-#        
-#        add_tag(
-#            self.browser,
-#            reverse('documents.views.list_documents'),
-#            tag,
-#        )
-#        
-#        document_list_url = \
-#            self.live_server_url + reverse('documents.views.list_documents')
-#        self.assertEquals(self.browser.url, document_list_url)
-#        
-#        last_tag_elem = b.find_by_css('span.taggit_tag').last
-#        last_tag_text = last_tag_elem.text
-#        
-#        
-##        document_xpath = '/html/body/div/div[2]/table/tbody/tr[1]'
-##        document_tr = self.browser.find_by_xpath(document_xpath)
-##        document_id = document_tr['data-id']
-##        document = Document.objects.get(id=document_id)
-##        
-##        self.assertEquals(document.public, public)
-##        self.assertEquals(document.title, title)
-##        self.assertEquals(document.notes, notes)
-##        
-##        document_title_xpath = '//*[@id="documents_cell"]/span[1]'
-##        document_title_elems = self.browser.find_by_xpath(document_title_xpath)
-##        document_title_first = document_title_elems.first
-##        privacy_icon_xpath = '//*[@id="privacy"]/i'
-##        privacy_icon_elems = self.browser.find_by_xpath(privacy_icon_xpath)
-##        privacy_icon_first = privacy_icon_elems.first
-##        
-##        self.assertEquals(document_title_first.value, title)
-##        self.assertTrue(privacy_icon_first.has_class('icon-eye-open'))
-##        
-###        import time; time.sleep(3)
-##        self.browser.quit()
+from documents import fss_utils as fss
+from documents.models import Document
+from documents.tests import disconnect
+from docviewer.models import document_delete, document_save
+from utils import create_user, exists_tag, get_document, get_tag
+from test_user import check_permissions, login
+from test_document import upload, set_site, process_document, get_abs_path
 
 
-#def add_tag(browser, list_url, tag):
-#    browser.click_link_by_partial_href(list_url)
-#    
-#    browser.find_by_css('tr.document-row').first.mouse_over()
-#    browser.fill('taggit_tag', tag + '\r')
+class TagTest(StaticLiveServerTestCase):
+    def setUp(self):
+        fss.remove_tree(settings.MEDIA_ROOT)
+        disconnect(post_save, document_save)
+        check_permissions()
+        set_site(self.live_server_url)
+        
+        self.browser = Browser()
+        self.browser.visit(self.live_server_url)
+        
+        login_url = settings.LOGIN_URL
+        self.browser.click_link_by_partial_href(login_url)
+        
+        username = 'antonio'
+        password = 'antonio'
+        create_user(username)
+        login(
+            self.browser,
+            username,
+            password,
+        )
+        
+        upload_url = reverse('documents.views.add_document')
+        self.browser.click_link_by_partial_href(upload_url)
+        
+        source = 'local'
+        docfile = get_abs_path('doctest.pdf')
+        language = 'eng'
+        public = True
+        title = 'test'
+        notes = 'test notes'
+        upload(
+            self.browser,
+            source,
+            docfile,
+            language,
+            public,
+            title,
+            notes,
+        )
+        
+        document = get_document(title)
+        process_document(document.id)
+        self.browser.is_element_not_present_by_value('ready', 10)
+        
+        tag = 'tag'
+        add_tag(
+            self.browser,
+            tag,
+        )
+        
+        self.tag = tag
+        self.tag_obj = get_tag(tag)
+    
+    def test_add_tag(self):
+        tag_exists = exists_tag(self.tag)
+        self.assertTrue(tag_exists)
+        self.assertEquals(self.tag_obj.name, self.tag)
+        
+        document_list_url = \
+            self.live_server_url + reverse('documents.views.list_documents')
+        self.assertEquals(self.browser.url, document_list_url)
+        
+        tag_span = self.browser.find_by_css('span.taggit_tag')
+        self.assertEquals(tag_span.value, self.tag)
+        
+#        import time; time.sleep(3)
+        self.browser.quit()
+    
+    def test_add_different_tag(self):
+        old_tag_num = len(self.browser.find_by_css('span.taggit_tag'))
+        
+        tag = 'other'
+        add_tag(
+            self.browser,
+            tag,
+        )
+        
+        new_tag_num = len(self.browser.find_by_css('span.taggit_tag'))
+        self.assertEquals(new_tag_num, old_tag_num + 1)
+        
+#        import time; time.sleep(3)
+        self.browser.quit()
+    
+    def test_add_same_tag(self):
+        old_tag_num = len(self.browser.find_by_css('span.taggit_tag'))
+        
+        tag = self.tag
+        add_tag(
+            self.browser,
+            tag,
+        )
+        
+        new_tag_num = len(self.browser.find_by_css('span.taggit_tag'))
+        self.assertEquals(new_tag_num, old_tag_num)
+        
+#        import time; time.sleep(3)
+        self.browser.quit()
+    
+    def test_remove_tag(self):
+        old_tag_num = len(self.browser.find_by_css('span.taggit_tag'))
+        
+        driver = self.browser.driver
+        actions = ActionChains(driver)
+        tag_link = driver.find_element_by_css_selector('#taggit_tags a')
+        actions.move_to_element(tag_link)
+        actions.move_by_offset(25, 10)
+        actions.click()
+        actions.perform()
+        
+        document_list_url = \
+            self.live_server_url + reverse('documents.views.list_documents')
+        self.assertEquals(self.browser.url, document_list_url)
+        
+        new_tag_num = len(self.browser.find_by_css('span.taggit_tag'))
+        self.assertEquals(new_tag_num, old_tag_num - 1)
+        
+#        import time; time.sleep(3)
+        self.browser.quit()
+
+
+def add_tag(browser, tag):
+    browser.find_by_css('tr.document-row').mouse_over()
+    browser.fill('taggit_tag', tag + '\r')
+    import time; time.sleep(1)
