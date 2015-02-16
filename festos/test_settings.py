a43d40996fbf4708da51718b9d4ec5bafb34d9de
mavillard <ajimene6@uwo.ca>
2015-02-15 21:48:44 -0500

    Enabling tests with celery

--- festos/test_settings.py
+++ festos/test_settings.py
@@ -9,8 +9,8 @@ DATABASES['default']['TEST_NAME'] = ':memory:'
 STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineStorage'
 PIPELINE_ENABLED = False
 
-#CELERY_EAGER_PROPAGATES_EXCEPTIONS=True
-#CELERY_ALWAYS_EAGER=True
-#BROKER_BACKEND='default'
+CELERY_EAGER_PROPAGATES_EXCEPTIONS=True
+CELERY_ALWAYS_EAGER=True
+BROKER_BACKEND='default'
 
 #TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
